52a7638d39cfd8f13c0b8e1078ff575c0137066f
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-30 02:02:43 -0400

    commit tree 4177b36fdcd0779bf326c634143fee177a253e14

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(driverArray) {
+    const firstTwoDrivers =  driverArray.slice(0 , 2)
+    return firstTwoDrivers
+};
+
+function returnLastTwoDrivers(driverArray) {
+    const lastTwoDrivers = driverArray.slice(Math.max(driverArray.length - 2, 0))
+    return lastTwoDrivers
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer) {
+    return function(integer) {
+      return integer * integer  
+    }
+    
+};
+
+function fareDoubler(integer) {
+    return integer * 2
+    
+};
+
+function fareTripler(integer) {
+    return integer * 3
+    
+};
+
+function selectDifferentDrivers(array, fn) {
+    return fn(array)
+    
+};
